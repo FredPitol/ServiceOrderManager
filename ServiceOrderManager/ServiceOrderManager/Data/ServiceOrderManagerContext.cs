3b578a012689fd404bf92b7d627c09f0727c1668
Fred Pitol <freirefrederico1@gmail.com>
2024-08-14 15:41:13 -0300

    Scaffold client

---
+++ ServiceOrderManager/ServiceOrderManager/Data/ServiceOrderManagerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ServiceOrderManager.Models;
+
+namespace ServiceOrderManager.Data
+{
+    public class ServiceOrderManagerContext : DbContext
+    {
+        public ServiceOrderManagerContext (DbContextOptions<ServiceOrderManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ServiceOrderManager.Models.Client> Client { get; set; }
+    }
+}
